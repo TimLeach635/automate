7a50a4d1462c1390c58f6fc8fe2b49c3906fc493
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-06-22 21:13:42 +0100

    Put a purple rectangle on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle}
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let rectangle = Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0)));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: rectangle,
+        material: materials.add(Color::PURPLE),
+        ..default()
+    });
 }
